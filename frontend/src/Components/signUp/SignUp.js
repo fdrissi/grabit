bb256a2a46051f6f3105009a26ed9bedb530a3f3
Fadel <drissi.toubbali.fadel@gmail.com>
2020-02-11 09:56:29 +0100

    Add Sign up page

---
+++ frontend/src/Components/signUp/SignUp.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import SignUpCard from "./SignUpCard";
+import { makeStyles, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    height: "300px"
+  },
+  text: {
+    height: "22px",
+  	width: "100%",
+  	color: "#222A30",
+  	fontFamily: "Montserrat",
+  	fontSize: "18px",
+  	fontWeight: "600",
+    lineHeight: "22px",
+    marginBottom: "20px"
+  }
+})
+
+export default () => {
+  const classes = useStyles()
+  // Get user type from url
+  const pathname = window.location.pathname;
+  const pathArray = pathname.split('/').filter(arr => arr !== "");
+  const userType = pathArray.pop();
+  console.log(userType)
+
+  return (
+    <Grid container direction="column" alignItems="center" justify="center" className={classes.container}>
+      <Grid item style={{ margin: "0 auto" }}>
+        <SignUpCard userType={userType}>
+          <div className={classes.text}>
+            <p >Sign up as {userType}</p>
+          </div>
+          
+        </ SignUpCard>
+      </Grid>
+    </Grid>
+  )
+}
---
+++ frontend/src/Components/signUp/SignUpCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper, Button} from '@material-ui/core/';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+    width: "100%"
+  },
+  button: {
+    backgroundColor: "#1976F3"
+  }
+}));
+
+export default function SimplePaper({ children, userType }) {
+  const classes = useStyles();
+  // Get user type from url
+
+  return (
+    <div className={classes.root}>
+      <Paper elevation={0} style={{  width: "500px", height: "200px", padding: "50px 95px" }} >
+        {children}
+        <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        className={classes.button}
+        startIcon={<img src="/img/facebook.png" alt="facebook" style={{ width: "38px", height: "38px" }} />}
+        href={`http://localhost:5000/api/v1/auth/facebook/login/${userType}`}
+        >
+          Continue with Facebook
+        </Button>
+      </ Paper>
+    </div>
+  );
+}

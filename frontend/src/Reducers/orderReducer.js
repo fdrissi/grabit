694e325d75a68c95e6b4b63111b7bcfe6df46e09
fdrissi- <drissi.toubbali.fadel@gmail.com>
2020-03-03 11:00:05 +0100

    Order reducer

--- frontend/src/Reducers/orderReducer.js
+++ frontend/src/Reducers/orderReducer.js
@@ -1,7 +1,11 @@
-import { SET_ORDER, UNSET_ORDER } from "../Actions/actionTypes";
+import { SET_ORDER, UNSET_ORDER, SET_DRIVERS, UPDATE_ORDER, ASSIGN_DRIVER } from "../Actions/actionTypes";
 
 export const orderInitState = {
   details: {},
+  drivers: [],
+  driver: {},
+  assignedDriver: false,
+  orderConfirmed: false,
   set: false,
 };
 
@@ -9,10 +13,16 @@ export const orderReducer = (state = orderInitState, action) => {
   const { type, payload } = action;
   switch (type) {
     case SET_ORDER:
-      return { details: payload, set: true }
+      return { ...state, details: payload, set: true }
+    case SET_DRIVERS:
+      return { ...state, drivers: payload }
+    case ASSIGN_DRIVER:
+      return { ...state, driver: payload }
+    case UPDATE_ORDER:
+      return { ...state, orderConfirmed: true, details: { ...state.details, ...payload } }
     
     case UNSET_ORDER:
-      return { details: {}, set: false }
+      return { details: {}, drivers: [], assignedDriver: {}, set: false }
     default:
       return state;
   }
